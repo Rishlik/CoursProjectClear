1275f175b814d7287ba1e5c44ff3bf5bf2a7650b
Rishli <kira.chudnyh@gmail.com>
2021-06-09 13:09:59 +0300

    Добавьте файлы проекта.

---
+++ CoursProjectClear/Classes/Navig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursProjectClear.Classes
+{
+    class Navig
+    {
+        public static System.Windows.Controls.Frame MainFrame { get; set; }
+    }
+}
---
+++ CoursProjectClear/Pages/AutoPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CoursProjectClear.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoursProjectClear.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AutoPage.xaml
+    /// </summary>
+    public partial class AutoPage : Page
+    {
+        public AutoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Auto_Click(object sender, RoutedEventArgs e)
+        {
+            Navig.MainFrame.Navigate(new SectionsPage());
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            Navig.MainFrame.Navigate(new RegistPage());
+        }
+    }
+}
